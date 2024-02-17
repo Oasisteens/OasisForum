1306084209b26e1405c44b4fd30a08be7a0ce682
LQ <143152892+LQ458@users.noreply.github.com>
2024-02-17 16:18:26 +0800

    LikeButton v2

--- components/likeButton.jsx
+++ components/likeButton.jsx
@@ -1,47 +1,50 @@
-'use client'
+"use client";
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
+import numeral from "numeral";
 
 const likeButton = ({
-    likeloads,
-    like,
-    postId,
-    likestatuses,
-    username,
-    setLikestatuses,
-    setLikes,
-    size,
-    category,
-    fontsize,
-    shift
+  likeloads,
+  like,
+  postId,
+  likestatuses,
+  username,
+  setLikestatuses,
+  setLikes,
+  size,
+  category,
+  fontsize,
+  shift,
+  height,
+  vershift,
 }) => {
-    const [likeload, setLikeLoad] = useState(false);
-    const sendLike = async (category, e) => {
-        e.preventDefault();
-        try {
-          setLikeLoad(true);
-          const likestatus = likestatuses?.find(
-            (likestatus) => likestatus.postId === postId,
-          );
-          const currentStatus = likestatus?.status ?? false;
-          const res = await axios.post("/api/fetchLike", {
-            postId,
-            sendUsername: username,
-            status: !currentStatus,
-            category,
-          });
-          setLikestatuses(res.data.likestatuses);
-          setLikes(res.data.likes);
-          setLikeLoad(false);
-        } catch (error) {
-          console.log(error);
-        }
-      };
+  const [likeload, setLikeLoad] = useState(false);
+  const sendLike = async (category, e) => {
+    e.preventDefault();
+    try {
+      setLikeLoad(true);
+      const likestatus = likestatuses?.find(
+        (likestatus) => likestatus.postId === postId,
+      );
+      const currentStatus = likestatus?.status ?? false;
+      const res = await axios.post("/api/fetchLike", {
+        postId,
+        sendUsername: username,
+        status: !currentStatus,
+        category,
+      });
+      setLikestatuses(res.data.likestatuses);
+      setLikes(res.data.likes);
+      setLikeLoad(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       {like.postId === postId && !(likeloads || likeload) && (
-        <section style={{ display: "flex" }}>
+        <section style={{ display: "flex", height: height }}>
           <form
             onSubmit={(e) => sendLike(category, e)}
             disabled={likeloads || likeload}
@@ -85,12 +88,19 @@ const likeButton = ({
               })()}
             </button>
           </form>
-          <p className="postlike" style={{fontSize: fontsize, right: shift, position: "relative"}}>{like.number}</p>
+          <p
+            className="postlike"
+            style={{ fontSize: fontsize, left: shift, top: vershift }}
+          >
+            {like.number > 1000
+              ? numeral(like.number).format("0.0a")
+              : like.number}
+          </p>
         </section>
       )}
 
       {like.postId === postId && (likeloads || likeload) && (
-        <div className="likeLoad">
+        <section className="likeLoad" style={{ height: height }}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="red"
@@ -101,7 +111,7 @@ const likeButton = ({
           >
             <path d="M256 448a32 32 0 01-18-5.57c-78.59-53.35-112.62-89.93-131.39-112.8-40-48.75-59.15-98.8-58.61-153C48.63 114.52 98.46 64 159.08 64c44.08 0 74.61 24.83 92.39 45.51a6 6 0 009.06 0C278.31 88.81 308.84 64 352.92 64c60.62 0 110.45 50.52 111.08 112.64.54 54.21-18.63 104.26-58.61 153-18.77 22.87-52.8 59.45-131.39 112.8a32 32 0 01-18 5.56z" />
           </svg>
-        </div>
+        </section>
       )}
     </>
   );
